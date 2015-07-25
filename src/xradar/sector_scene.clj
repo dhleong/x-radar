53a6b7bde5c3d9270f1edc52f8f834053868a8b0
dhleong <falcone88@gmail.com>
2015-07-25 18:02:52 -0400

    Basic support for .sct2 reading
    Could be more performant; we don't read all sections yet, and
    ZNY takes ~5 seconds to load. We support:
    
          :info
          :vor
          :ndb
          :airport
          :geo
          :labels
    
    Now we need to figure out how to draw these

--- src/xradar/sector_scene.clj
+++ src/xradar/sector_scene.clj
@@ -1,11 +1,24 @@
 (ns ^{:author "Daniel Leong"
       :doc "XScene implementation for .sct2 files"}
   xradar.sector-scene
-  (:require [clojure.string :refer [split]]
+  (:require [clojure.string :refer [lower-case split]]
             [clojure.java.io :as io]
             [quil
              [core :as q]]
             [xradar.scene :refer [XScene draw-scene]]))
+
+;;
+;; Constants
+;;
+(def re-spaces #"\s+")
+
+;;
+;; Util methods
+;;
+
+(defn as-int
+  [raw]
+  (Integer/parseInt raw))
 
 (defn parse-color
   [raw]
@@ -22,22 +35,24 @@
       ;; default to black, I guess?
       0xff000000)))
 
-(defn- parse-data-line
-  [data line]
-  (cond
-    ;; define
-    (.startsWith line "#define")
-    (let [parts (split line #" ")]
-      (assoc-in data
-                [:colors (keyword (second parts))]
-                (parse-color (last parts))))
-    ;; mode line
-    (.startsWith line "[")
-    (assoc data :mode- (.substring line 
-                          1
-                          (.indexOf line "]")))
-    ;; TODO
-    ))
+(defn parse-coord
+  ([coord]
+   (let [sign
+         (case (first coord)
+           \N 1
+           \E 1
+           \S -1
+           \W -1)
+         parts (-> coord
+                   (.substring 1)
+                   (split #"\.")
+                   (#(map as-int %)))]
+     (* sign (+ (nth parts 0) ; degrees
+                (/ (nth parts 1) 60) ; minutes
+                (/ (nth parts 2) 3600) ; seconds
+                (/ (nth parts 3) 3600000))))) ; decimal seconds
+  ([lat lon]
+   {:x (parse-coord lon) :y (parse-coord lat)}))
 
 (defn- clean-line
   [line]
@@ -46,12 +61,138 @@
       (-> line (.substring 0 comment-idx))
       line)))
 
+;;
+;; Sections
+;;
+
+(defn- parse-info-line
+  "[INFO]"
+  [data line]
+  (let [info-line (or (:info-line- data) 0)
+        [k v] 
+        (case info-line
+          0 [:name line]
+          1 [:default-callsign line]
+          2 [:default-airport line]
+          3 [:center-lat line]
+          4 [:center-lon line]
+          5 [:nm-per-lat (Integer/parseInt line)]
+          6 [:nm-per-lon (Integer/parseInt line)]
+          7 [:magnet-var (Double/parseDouble line)]
+          8 [:sect-scale line])]
+    (assoc-in (assoc data :info-line- (inc info-line))
+              [:info k] v)))
+
+(defn- parse-point-line
+  "[VOR] or [NDB]"
+  [section data line]
+  (let [parts (split line re-spaces)
+        info
+        {:name (first parts)
+         :freq (second parts)
+         :coord (parse-coord (nth parts 2)
+                             (nth parts 3))}]
+    (assoc data 
+           section 
+           (conj (get data section []) info))))
+
+(defn- parse-airport-line
+  "[AIRPORT]"
+  [data line]
+  (let [parts (split line re-spaces)
+        info
+        {:name (first parts)
+         :freq (second parts)
+         :coord (parse-coord (nth parts 2)
+                             (nth parts 3))
+         :airspace (last parts)}]
+    (assoc data 
+           :airport 
+           (conj (get data :airport []) info))))
+
+(defn- parse-geo-line
+  "[GEO]"
+  [data line]
+  (let [parts (split line re-spaces)
+        color (keyword (last parts))
+        info
+        (try
+          {:start (parse-coord (nth parts 0)
+                               (nth parts 1))
+           :end (parse-coord (nth parts 2)
+                             (nth parts 3))
+           :color (get-in data 
+                          [:colors color] 
+                          (parse-color (last parts)))}
+          (catch IllegalArgumentException e
+            ;; ZNY prefixes each group with a line
+            ;;  referring to the airport's name....
+            nil))]
+    (assoc data 
+           :geo 
+           (conj (get data :geo []) info))))
+
+(defn- parse-label-line
+  "[LABELS]"
+  [data line]
+  (let [label-end (-> line (.indexOf "\"" 1))
+        label (-> line (.substring 1 label-end))
+        line-info (-> line
+                      (.substring (inc label-end))
+                      (.trim))
+        parts (split line-info re-spaces)
+        color (keyword (last parts))
+        info
+        {:label label
+         :coord (parse-coord (nth parts 0)
+                             (nth parts 1))
+         :color (get-in data 
+                        [:colors color] 
+                        (parse-color (last parts)))}]
+    (assoc data 
+           :labels
+           (conj (get data :labels []) info))))
+
+;;
+;; Parsing loop
+;;
+
+(defn- parse-data-line
+  [data line]
+  (cond
+    ;; define
+    (.startsWith line "#define")
+    (let [parts (split line re-spaces)]
+      (assoc-in data
+                [:colors (keyword (second parts))]
+                (parse-color (last parts))))
+    ;; mode line
+    (.startsWith line "[")
+    (assoc data :mode- (-> line
+                           (.substring 1 (.indexOf line "]"))
+                           lower-case
+                           (.replace " " "-")
+                           keyword))
+    ;; data line
+    (not (or (empty? line) (.startsWith line ";")))
+    (case (:mode- data)
+      :info (parse-info-line data line)
+      :vor (parse-point-line :vor data line)
+      :ndb (parse-point-line :ndb data line)
+      :airport (parse-airport-line data line)
+      :geo (parse-geo-line data line)
+      :labels (parse-label-line data line)
+      ;; unsupported section
+      nil)))
+
 (defn- load-from-reader
   [reader]
   (loop [lines (line-seq reader)
          data {}]
     (if (empty? lines)
-      data
+      ;; return what we have
+      (dissoc data :mode- :info-line-)
+      ;; parse it!
       (if-let [updated (parse-data-line data (clean-line (first lines)))]
         ; success!
         (recur (rest lines)
@@ -60,15 +201,19 @@
         (recur (rest lines)
                data)))))
 
+(defn load-sector-data [input]
+  (with-open [reader (io/reader input)]
+    (load-from-reader reader)))
+
+;;
+;; Public interface
+;;
+
 (deftype SectorScene [data]
   XScene
   (draw-scene [this profile]
     ;; FIXME draw the scene
     nil))
-
-(defn load-sector-data [input]
-  (with-open [reader (io/reader input)]
-    (load-from-reader reader)))
 
 (defn load-sector [input]
   (->SectorScene (load-sector-data input)))
--- test/xradar/sector_scene_test.clj
+++ test/xradar/sector_scene_test.clj
@@ -2,9 +2,36 @@
   (:require [clojure.test :refer :all]
             [xradar.sector-scene :refer :all]))
 
+;;
+;; Consts
+;;
+
+(def data-info
+  "[INFO]\nBoston Tower v5.0_FC\nBOS_TWR\nKBOS\nN042.20.54.750\nW071.00.21.920\n60\n45\n16\n1")
+
+(def data-geo
+  (str "#define Taxiway 14737632\n"
+       "[GEO]\n"
+       "N042.00.00.000 W071.00.00.000 N042.00.00.000 W071.00.00.000 Taxiway\n"
+       "N042.00.00.000 W071.00.00.000 N042.00.00.000 W071.00.00.000 255" ))
+
+(def data-labels
+  (str "#define Taxiway 14737632\n"
+       "[LABELS]\n"
+       "\"S\" N042.00.00.000 W071.00.00.000 Taxiway\n"
+       "\"Awesome Spot\" N042.00.00.000 W071.00.00.000 255"))
+
+;;
+;; Utils
+;;
+
 (defn load-data
   [string]
   (load-sector-data (java.io.StringReader. string)))
+
+;; 
+;; Tests
+;;
 
 (deftest utils-tests
   (testing "parse-color"
@@ -13,9 +40,50 @@
     (is (= 0xff0000FF (parse-color "16711680")))
     ;; colors from ZNY
     (is (= 0xff606060 (parse-color "6316128")))
-    (is (= 0xff142A00 (parse-color "10772")))))
+    (is (= 0xff142A00 (parse-color "10772"))))
+  (testing "parse-coord"
+    (is (== 152478037/3600000 (parse-coord "N042.21.18.037")))
+    (is (== -42.355 (parse-coord "S042.21.18.000")))
+    (is (= {:x 42 :y -42} 
+           (parse-coord "S042.00.00.000"
+                        "E042.00.00.000")))))
 
 (deftest load-sector-test
   (testing "#define"
     (let [data (load-data "#define Foo 255")]
-      (is (= 0xffFF0000 (-> data :colors :Foo))))))
+      (is (= 0xffFF0000 (-> data :colors :Foo)))))
+  (testing "[INFO]"
+    (let [data (load-data data-info)]
+      (is (= "Boston Tower v5.0_FC" (-> data :info :name)))
+      (is (= 45 (-> data :info :nm-per-lon)))
+      (is (= 16 (-> data :info :magnet-var)))))
+  (testing "[VOR]"
+    (let [data (load-data "[VOR]\nBOS 112.700 N042.00.00.000 W070.00.00.000")]
+      (is (= [{:name "BOS"
+               :freq "112.700"
+               :coord {:x -70 :y 42}}] (-> data :vor)))))
+  (testing "[AIRPORT]"
+    (let [data 
+          (load-data "[AIRPORT]\nKBOS 128.800 N042.00.00.000 W070.00.00.000 B")]
+      (is (= [{:name "KBOS"
+               :freq "128.800"
+               :coord {:x -70 :y 42}
+               :airspace "B"}] (-> data :airport)))))
+  (testing "[GEO]"
+    (let [data (load-data data-geo)]
+      (is (= [{:start {:x -71 :y 42}
+               :end {:x -71 :y 42}
+               :color 0xffE0E0E0}
+              {:start {:x -71 :y 42}
+               :end {:x -71 :y 42}
+               :color 0xffFF0000}]
+             (-> data :geo)))))
+  (testing "[LABELS]"
+    (let [data (load-data data-labels)]
+      (is (= [{:label "S"
+               :coord {:x -71 :y 42}
+               :color 0xffE0E0E0}
+              {:label "Awesome Spot"
+               :coord {:x -71 :y 42}
+               :color 0xffFF0000}]
+             (-> data :labels))))))
