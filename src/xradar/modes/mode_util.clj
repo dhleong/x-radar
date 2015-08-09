f3192af4460b6202a369496867d7d0a0edb3e8f4
dhleong <falcone88@gmail.com>
2015-08-09 16:42:51 -0400

    Highlight the current char sequence in selection mode

--- src/xradar/modes/mode_util.clj
+++ src/xradar/modes/mode_util.clj
@@ -15,8 +15,13 @@
   Also handles drawing the mapping characters for selection."
   [radar scheme my-aircraft craft]
   (let [state (:state craft :untracked)
+        current-sequence (-> radar :input deref :current-sequence)
+        current-sequence-str (->> current-sequence
+                                  (map name)
+                                  (apply str))
+        selected-color (-> scheme :aircraft :selected)
         craft-color (if (:selected craft)
-                      (-> scheme :aircraft :selected)
+                      selected-color
                       (get-in scheme [:aircraft state]))
         mapping (if (= :select-aircraft (:mode radar))
                   (get-in radar [:craft-bindings (:cid craft)] nil))]
@@ -31,11 +36,17 @@
       (q/translate x y 0))
     (q/hint :disable-depth-test) ;; so we draw on top of scenery
     (my-aircraft scheme craft)
-    (when mapping
+    (when (and mapping
+               (.startsWith (str mapping) current-sequence-str))
       (q/fill-int craft-color)
       (q/text-size mapping-text-size)
       (q/text-align :right :center)
-      (q/text (str mapping) mapping-offset-x mapping-offset-y))
+      (q/text (str mapping) mapping-offset-x mapping-offset-y)
+      (def the-current-sequence current-sequence)
+      (def the-current-sequence-str current-sequence-str)
+      (when-not (empty? current-sequence-str)
+        (q/fill-int selected-color)
+        (q/text (str current-sequence-str " ") mapping-offset-x mapping-offset-y)))
     (q/hint :enable-depth-test)
     (q/pop-matrix)))
 
